a016ddb3d65952c77252fbda7c5dcf96b0724d6d
Teerrthan118 <94755143+Teerrthan118@users.noreply.github.com>
2022-02-24 17:03:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function questi(){
+    document.getElementById("yes").style.display="inline-block";
+    document.getElementById("is").style.display="none";
+    document.getElementById("question").style.display="none";
+}
